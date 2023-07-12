ce5b08256609b837eeee39abdbdd65f876e8dcef
Sebastian Ochoa <38465363+sochoa1705@users.noreply.github.com>
2023-07-12 13:32:39 -0500

    Spring Initialzr config

---
+++ src/test/java/ec/edu/espe/arquitectura/banquitowsgestionadmin/BanquitoWsGestionAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arquitectura.banquitowsgestionadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanquitoWsGestionAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
