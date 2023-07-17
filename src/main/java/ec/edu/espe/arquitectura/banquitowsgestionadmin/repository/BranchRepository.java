c7756af82cad3f4216f8344f04046625a2e58085
JeffNolasco <jeffersonalexander53@gmail.com>
2023-07-17 11:10:38 -0500

    branch crud
    branch crud v2

--- src/main/java/ec/edu/espe/arquitectura/banquitowsgestionadmin/repository/BranchRepository.java
+++
@@ -1,8 +0,0 @@
-package ec.edu.espe.arquitectura.banquitowsgestionadmin.repository;
-
-import ec.edu.espe.arquitectura.banquitowsgestionadmin.model.BankEntity;
-import ec.edu.espe.arquitectura.banquitowsgestionadmin.model.Branch;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface BranchRepository extends MongoRepository<Branch, String> {
-}
