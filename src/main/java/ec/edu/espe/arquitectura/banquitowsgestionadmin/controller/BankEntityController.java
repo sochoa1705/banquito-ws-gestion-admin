d17d1e2b2841f23af3e65c0f6144a38660958a4e
JeffNolasco <jeffersonalexander53@gmail.com>
2023-07-18 11:22:58 -0500

    crud branch v3
    crud branch v3

--- src/main/java/ec/edu/espe/arquitectura/banquitowsgestionadmin/controller/BankEntityController.java
+++ src/main/java/ec/edu/espe/arquitectura/banquitowsgestionadmin/controller/BankEntityController.java
@@ -1,5 +1,7 @@
 package ec.edu.espe.arquitectura.banquitowsgestionadmin.controller;
 
+import ec.edu.espe.arquitectura.banquitowsgestionadmin.controller.dto.BranchRQ;
+import ec.edu.espe.arquitectura.banquitowsgestionadmin.controller.dto.BranchRS;
 import ec.edu.espe.arquitectura.banquitowsgestionadmin.model.Branch;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -28,23 +30,23 @@ public class BankEntityController {
     }
 
     @GetMapping("/branch-list/{bankEntityId}")
-    public ResponseEntity<List<Branch>> findBranchesByBankEntityId(@PathVariable("bankEntityId") String id){
-        List<Branch> rs = this.service.listAllBranchesByEntity(id);
+    public ResponseEntity<List<BranchRS>> findBranchesByBankEntityId(@PathVariable("bankEntityId") String id){
+        List<BranchRS> rs = this.service.listAllBranchesByEntity(id);
         return ResponseEntity.ok(rs);
     }
 
     @GetMapping("/branch-unique/{bankEntityId}/{branchId}")
-    public ResponseEntity <Branch> findBranchByCode(@PathVariable("bankEntityId") String bankEntityId,
+    public ResponseEntity <BranchRS> findBranchByCode(@PathVariable("bankEntityId") String bankEntityId,
                                                     @PathVariable("branchId") String branchId){
-        Branch rs = this.service.BranchByCode(bankEntityId, branchId);
+        BranchRS rs = this.service.BranchByCode(bankEntityId, branchId);
         return ResponseEntity.ok(rs);
     }
 
     @PutMapping("/branch-create/{bankEntityId}")
     public ResponseEntity<BankEntity> addBranch(@PathVariable String bankEntityId,
-                                                @RequestBody Branch branch){
+                                                @RequestBody BranchRQ branchRQ){
         try {
-            BankEntity bankEntity = this.service.addBranch(bankEntityId, branch);
+            BankEntity bankEntity = this.service.addBranch(bankEntityId, branchRQ);
             return ResponseEntity.ok(bankEntity);
         }catch (RuntimeException rte){
             return ResponseEntity.badRequest().build();
@@ -54,9 +56,9 @@ public class BankEntityController {
     @PutMapping("/branch-update/{bankEntityId}/{branchId}")
     public ResponseEntity<BankEntity> updateBranch(@PathVariable String bankEntityId,
                                                 @PathVariable String branchId,
-                                                @RequestBody Branch branch){
+                                                @RequestBody BranchRQ branchRQ){
         try {
-            BankEntity bankEntity = this.service.updateBranch(bankEntityId, branchId, branch);
+            BankEntity bankEntity = this.service.updateBranch(bankEntityId, branchId, branchRQ);
             return ResponseEntity.ok(bankEntity);
         }catch (RuntimeException rte){
             return ResponseEntity.badRequest().build();
@@ -64,12 +66,12 @@ public class BankEntityController {
     }
 
     @PutMapping("/branch-delete/{bankEntityId}/{branchId}")
-    public ResponseEntity<BankEntity> deleteBranch(@PathVariable String bankEntityId,
-                                                   @PathVariable String branchId,
-                                                   @RequestBody Branch branch){
+    public ResponseEntity<BranchRS> deleteBranch(@PathVariable String bankEntityId,
+                                                   @PathVariable String branchId
+                                                   ){
         try {
-            BankEntity bankEntity = this.service.deleteBranch(bankEntityId, branchId, branch);
-            return ResponseEntity.ok(bankEntity);
+            BranchRS rs = this.service.deleteBranch(bankEntityId, branchId);
+            return ResponseEntity.ok(rs);
         }catch (RuntimeException rte){
             return ResponseEntity.badRequest().build();
         }
